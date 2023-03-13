4e8e220acd6cad3a2aff64ef97b1fedfe78a2ac1
kjpar0317 <kjpar0317@gmail.com>
2023-03-13 23:31:13 +0900

    Changes

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.6.21"
+	kotlin("jvm") version "1.8.0"
 }
 
 group = "com.kjpar0317"
@@ -15,8 +15,8 @@ repositories {
 
 dependencies {
 	implementation("io.javalin:javalin:5.4.2")
-	implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.0")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.0")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0")
 	implementation("org.slf4j:slf4j-simple:2.0.6")
 	implementation("com.zaxxer:HikariCP:5.0.1")
 }
